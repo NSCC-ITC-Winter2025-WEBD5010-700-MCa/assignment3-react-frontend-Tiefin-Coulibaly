fe87c6e41c7fc2c209e177cd6eec62beb45fec76
mamadou <ctiefin@gmail.com>
2025-02-24 15:00:25 -0400

    ICA Feb 24, 2025

--- src/components/books/BookCreate.jsx
+++ src/components/books/BookCreate.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 import { useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+import BookForm from "./BookForm";
 
 const BookCreate = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
 
-  const collectData = (data) => {
-    // createBookMutation.mutate(data);
-    console.log(data);
-  };
+
 
   const createBookMutation = useMutation({
     mutationFn: async (data) => {
@@ -34,6 +26,11 @@ const BookCreate = () => {
       navigate("..");
     },
   });
+
+  const processData = (data) => {
+    createBookMutation.mutate(data);
+  };
+
   return (
     <div>
       <h2>Create a new book</h2>
@@ -41,69 +38,7 @@ const BookCreate = () => {
         <h2 className="text-2xl font-bold mb-4 text-gray-800">
           Create New Book
         </h2>
-        <form onSubmit={handleSubmit(collectData)} className="space-y-4">
-          <div>
-            <input
-              {...register("title", { required: "Title is required!" })}
-              type="text"
-              placeholder="Title"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            {errors.title && (
-              <p className="text-red-500 text-sm mt-1">
-                {errors.title.message}
-              </p>
-            )}
-          </div>
-          <div>
-            <input
-              {...register("author", { required: "Author is required!" })}
-              type="text"
-              placeholder="Author"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            {errors.author && (
-              <p className="text-red-500 text-sm mt-1">
-                {errors.author.message}
-              </p>
-            )}
-          </div>
-          <div>
-            <input
-              {...register("published_year", {
-                required: "Year is required!",
-                min: { value: 1700, message: "Year must be greater than 1700" },
-              })}
-              type="number"
-              placeholder="Year"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            {errors.published_year && (
-              <p className="text-red-500 text-sm mt-1">
-                {errors.published_year.message}
-              </p>
-            )}
-          </div>
-          <div>
-            <input
-              {...register("genre", { required: "Genre is required!" })}
-              type="text"
-              placeholder="Genre"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            {errors.genre && (
-              <p className="text-red-500 text-sm mt-1">
-                {errors.genre.message}
-              </p>
-            )}
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-all"
-          >
-            Create Book
-          </button>
-        </form>
+        <BookForm onDataCollected={processData}/>
       </div>
     </div>
   );
